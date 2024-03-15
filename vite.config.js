2b8088d682ec03f4598a9148d8f3bd98437d9c43
ArcidesFerrao <cidesferrao@gmail.com>
2024-03-15 13:13:05 +0200

    add: vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/protfolio-project/",
+  
+  plugins: [
+    react(),
+    AutoImport({
+      resolvers: [
+        IconsResolver({
+          prefix: 'Icon',
+          extension: 'jsx',
+        }),
+      ],
+    }),
+    Icons({
+      compiler: 'jsx', // or 'solid'
+    }),
+  
+  ],
 })
